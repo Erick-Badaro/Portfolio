49fee3597f96d5bdb516bcb1d59d24bf690ba40e
Erick Badaró <zaterick10@gmail.com>
2026-03-05 02:09:31 -0300

    Criando serviço de envio de e-mails

--- backend/src/models/Download.js
+++ backend/src/models/Download.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const downloadSchema = new mongoose.Schema({
     date:{
---
+++ backend/src/services/emailService.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
+const sendDownloadNotification = async (downloadInfo) => {
+    const {userAgent, downloadDate} = downloadInfo;
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.NOTIFY_EMAIL,
+        subject: "📥 Novo Download do CV!",
+        html: `
+                <h2> Alguém baixou seu CV!</h2>
+                <p><strong> Data:</strong> ${downloadDate.toLocaleString("pt-BR")}</p>
+                <p><strong>Navegador:</strong> ${userAgent}</p>        
+        `,
+    };
+
+    await transporter.sendMail(mailOptions);
+};
+
+module.exports = {sendDownloadNotification};
