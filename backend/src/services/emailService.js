2ac3af49570d97e7e5fcea6ee5cbd1646896f5d8
Erick Badaró <zaterick10@gmail.com>
2026-03-05 16:49:42 -0300

    Alterando formatação data

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -3,18 +3,20 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendDownloadNotification = async (downloadInfo) => {
-    const { userAgent, downloadDate } = downloadInfo;
+  const { userAgent, downloadDate } = downloadInfo;
 
-    await resend.emails.send({
-        from: "Portfolio <onboarding@resend.dev>",
-        to: process.env.NOTIFY_EMAIL,
-        subject: "📥 Novo Download do CV!",
-        html: `
+  await resend.emails.send({
+    from: "Portfolio <onboarding@resend.dev>",
+    to: process.env.NOTIFY_EMAIL,
+    subject: "📥 Novo Download do CV!",
+    html: `
             <h2>Alguém baixou seu CV!</h2>
-            <p><strong>Data:</strong> ${downloadDate.toLocaleString("pt-BR")}</p>
+            <p><strong>Data:</strong> ${downloadDate.toLocaleString("pt-BR", {
+              timeZone: "America/Sao_Paulo",
+            })}</p>
             <p><strong>Navegador:</strong> ${userAgent}</p>        
         `,
-    });
+  });
 };
 
 module.exports = { sendDownloadNotification };
